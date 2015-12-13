f7e1c4c017ded2e3ed38d11cf65bdc016e695a34
Colin Jones <colinsjones11@gmail.com>
2015-12-13 12:59:54 -0600

    change to instance variables

--- app.rb
+++ app.rb
@@ -1,28 +1,29 @@
 require 'sinatra'
 require 'json'
+require 'open-uri'
 
 post '/gateway' do
 
   return if params[:token] != 'LK1QyaEb4i9TGfd0Nb2adL8D'
 
-  trigger_word = params[:trigger_word].strip
-  poof_receiver = params[:text].gsub(trigger_word, '').strip
-  poof_giver = params[:user_name].strip
+  @trigger_word = params[:trigger_word].strip
+  @poof_receiver = params[:text].gsub(trigger_word, '').strip
+  @poof_giver = params[:user_name].strip
 
-  # make sure poof_receiver has @ symbol appended to front
-  if poof_receiver.include? "@"
-  else poof_receiver = poof_receiver.prepend("@")
+  # make sure @poof_receiver has @ symbol appended to front
+  if @poof_receiver.include? "@"
+  else @poof_receiver = @poof_receiver.prepend("@")
   end
 
 
-  # make sure poof_receiver is a slack user and formulate response_message
+  # make sure @poof_receiver is a slack user and formulate response_message
   # case poof_receiver
   #   when ''
   #   else # default - ignore
   # end
 
-  response_message = "#{poof_receiver} got a poof! :poof:\n#{poof_receiver} now has x total poofs"
+  @response_message = "#{@poof_receiver} got a poof! :poof:\n(#{@poof_receiver} now has x total poofs)"
 
   content_type :json
-  {:username => 'poofit', :response_type => "in-channel", :text => response_message }.to_json
+  {:username => 'poofit', :response_type => "in-channel", :text => @response_message }.to_json
 end

5c118ef9f1b9c5f2bf35d6fdf6b6269e7751458a
Colin Jones <colinsjones11@gmail.com>
2015-12-13 12:32:40 -0600

    add support for leaving out the @

--- app.rb
+++ app.rb
@@ -6,8 +6,13 @@ post '/gateway' do
   return if params[:token] != 'LK1QyaEb4i9TGfd0Nb2adL8D'
 
   trigger_word = params[:trigger_word].strip
-  message = params[:text].gsub(trigger_word, '').strip
+  poof_receiver = params[:text].gsub(trigger_word, '').strip
   user_name = params[:user_name].strip
+
+  if poof_receiver.include? "@"
+  else poof_receiver = "@" + poof_receiver
+  end
+
 
   #switch on the message
   # case user_name
@@ -15,7 +20,7 @@ post '/gateway' do
   #   else # default - ignore
   # end
 
-  response_message = "#{message} got a poof! :poof:"  #default response
+  response_message = "#{poof_receiver} got a poof! :poof:"
 
   content_type :json
   {:username => 'poofit', :response_type => "in-channel", :text => response_message }.to_json

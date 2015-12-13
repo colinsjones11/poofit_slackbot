173893ee4be67d61509d48a2f7a96964ab1b0b23
Colin Jones <colinsjones11@gmail.com>
2015-12-13 13:59:45 -0600

    refactored slack user check

--- app.rb
+++ app.rb
@@ -8,33 +8,33 @@ post '/gateway' do
 
   trigger_word = params[:trigger_word].strip
   poof_giver = params[:user_name].strip
-  poof_receiver = params[:text].gsub(trigger_word, '').strip
+  @poof_receiver = params[:text].gsub(trigger_word, '').strip
 
 
-  # make sure @poof_receiver is a slack user and formulate response message
-  di_slack_token = "xoxp-2151774549-4117477680-16044766884-54e7959db7"
-  slack_api_call = "https://slack.com/api/users.list?token=#{di_slack_token}&pretty=1"
-  raw_slack_data = open(slack_api_call).read
-  parsed_slack_data = JSON.parse(raw_slack_data)
-  @slack_users = []
-  parsed_slack_data["members"].each do |member|
-    @slack_users << member["name"]
-  end
-
-  case poof_receiver
-    when @slack_users.include? poof_receiver
-      response_message = "#{poof_receiver} got a poof! :poof:\n(#{poof_receiver} now has x total poofs)"
+# make sure @poof_receiver is a slack user and formulate response message
+    di_slack_token = "xoxp-2151774549-4117477680-16044766884-54e7959db7"
+    slack_api_call = "https://slack.com/api/users.list?token=#{di_slack_token}&pretty=1"
+    raw_slack_data = open(slack_api_call).read
+    parsed_slack_data = JSON.parse(raw_slack_data)
+    @slack_users = []
+    parsed_slack_data["members"].each do |member|
+      @slack_users << member["name"]
+    end
+    if @slack_users.include?(@poof_receiver)
+      # make sure poof_receiver has @ symbol prepended to front for messaging format purposes
+      if @poof_receiver.include? "@"
+      else @poof_receiver = @poof_receiver.prepend("@")
+      end
+      @response_message = "#{@poof_receiver} got a poof! :poof:\n(#{@poof_receiver} now has x total poofs)"
     else
-      response_message = "#{poof_receiver} is not a valid slack user :dizzy_face:"
-  end
+      # make sure poof_receiver has @ symbol prepended to front for messaging format purposes
+      if @poof_receiver.include? "@"
+      else @poof_receiver = @poof_receiver.prepend("@")
+      end
+      @response_message = "#{@poof_receiver} is not a valid slack user :dizzy_face:"
+    end
 
-
-
-  # make sure poof_receiver has @ symbol prepended to front
-  if poof_receiver.include? "@"
-  else poof_receiver = poof_receiver.prepend("@")
-  end
 
   content_type :json
-  {:username => 'poofit', :response_type => "in-channel", :text => response_message }.to_json
+  {:username => 'poofit', :response_type => "in-channel", :text => @response_message }.to_json
 end

703601c9c0c5a968035d3c8e90c9edf78dee6111
Colin Jones <colinsjones11@gmail.com>
2015-12-13 12:07:45 -0600

    added username to response

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'json'
 post '/gateway' do
   trigger_word = params[:trigger_word].strip
   message = params[:text].gsub(trigger_word, '').strip
+  user_name = params[:user_name].strip
 
   #switch on the message
   # case message
@@ -11,7 +12,7 @@ post '/gateway' do
   #   else # default - ignore
   # end
 
-  response_message = "Here's a poof! :poof:"  #default response
+  response_message = "#{user_name} got a poof! :poof:"  #default response
 
   content_type :json
   {:username => 'poofit', :response_type => "in-channel", :text => response_message }.to_json

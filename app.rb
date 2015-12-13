bb9ad4e4a91376a97cce0dbc40131bace830f7d7
Colin Jones <colinsjones11@gmail.com>
2015-12-13 14:13:07 -0600

    figuring out variables - rollback of previous commit

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ post '/gateway' do
 
   return if params[:token] != 'LK1QyaEb4i9TGfd0Nb2adL8D'
 
-  trigger_word = params[:trigger_word].strip
-  poof_giver = params[:user_name].strip
-  poof_receiver = params[:text].gsub(trigger_word, '').strip
+  @trigger_word = params[:trigger_word].strip
+  @poof_giver = params[:user_name].strip
+  @poof_receiver = params[:text].gsub(trigger_word, '').strip
 
 
   # make sure @poof_receiver is a slack user and formulate response message
@@ -16,17 +16,17 @@ post '/gateway' do
   slack_api_call = "https://slack.com/api/users.list?token=#{di_slack_token}&pretty=1"
   raw_slack_data = open(slack_api_call).read
   parsed_slack_data = JSON.parse(raw_slack_data)
-  slack_users = []
+  @slack_users = []
   parsed_slack_data["members"].each do |member|
-    slack_users << member["name"]
+    @slack_users << member["name"]
   end
 
-  if slack_users.include?(poof_receiver)
-    response_message = "#{poof_receiver} got a poof! :poof:\n(#{poof_receiver} now has x total poofs)"
+  if @slack_users.include?(@poof_receiver)
+    @response_message = "#{@poof_receiver} got a poof! :poof:\n(#{@poof_receiver} now has x total poofs)"
   else
-    response_message = "#{poof_receiver} is not a valid slack user :dizzy_face:"
+    @response_message = "#{@poof_receiver} is not a valid slack user :dizzy_face:"
   end
 
-  content_type :json
-  {:username => 'poofit', :response_type => "in-channel", :text => response_message }.to_json
+
+  {:username => 'poofit', :response_type => "in-channel", :text => @response_message }.to_json
 end

c3a20fea2a234ac277ff6fcea170c69eae6eeb19
Colin Jones <colinsjones11@gmail.com>
2015-12-13 11:39:21 -0600

    changing to simple hello world message

--- app.rb
+++ app.rb
@@ -5,18 +5,30 @@ require 'json'
 post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
 
-  action, repo = message.split('_').map {|c| c.strip.downcase }
-  repo_url = "https://api.github.com/repos/#{repo}"
-
-  case action
-    when 'issues'
-      resp = HTTParty.get(repo_url)
-      resp = JSON.parse resp.body
-      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+  case message
+    when 'hello'
+      response = "hello there!"
+    end
   end
-end
 
-def respond_message message
+
+#   action, repo = message.split('_').map {|c| c.strip.downcase }
+#   repo_url = "https://api.github.com/repos/#{repo}"
+
+#   case action
+#     when 'issues'
+#       resp = HTTParty.get(repo_url)
+#       resp = JSON.parse resp.body
+#       respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+#   end
+# end
+
+# def respond_message message
+#   content_type :json
+#   {:text => message}.to_json
+# end
+
+def response
   content_type :json
-  {:text => message}.to_json
+  {:text => response}.to_json
 end

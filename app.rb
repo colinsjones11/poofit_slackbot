85bf7ae023405fbf2c02ef88e82aeeeda88986f6
Colin Jones <colinsjones11@gmail.com>
2015-12-13 14:25:03 -0600

    added add_at_symbol method and inline comments

--- app.rb
+++ app.rb
@@ -2,10 +2,20 @@ require 'sinatra'
 require 'json'
 require 'open-uri'
 
+# add at symbol to a name so slack recognizes
+def add_at_symbol(string)
+  if string.start_with?("@")
+    else string.prepend("@")
+  end
+end
+
+# listening on '/gateway' - defined in slack webhook config and heroku
 post '/gateway' do
 
+  # need to move this to an env variable
   return if params[:token] != 'LK1QyaEb4i9TGfd0Nb2adL8D'
 
+  # parsing out slack message components
   trigger_word = params[:trigger_word].strip
   poof_giver = params[:user_name].strip
   poof_receiver = params[:text].gsub(trigger_word, '').strip
@@ -22,9 +32,11 @@ post '/gateway' do
   end
 
   if slack_users.include?(poof_receiver)
+    poof_receiver.add_at_symbol
     response_message = "#{poof_receiver} got a poof! :poof:\n(#{poof_receiver} now has x total poofs)"
   else
-    response_message = "#{poof_receiver} is not a valid slack user :dizzy_face:"
+    poof_receiver.add_at_symbol
+    response_message = "#{poof_receiver} is not a member of this slack team :dizzy_face:"
   end
 
 

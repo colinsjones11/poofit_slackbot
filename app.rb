16680abd22e168ee82faf13742f72f986108e7f5
Colin Jones <colinsjones11@gmail.com>
2015-12-13 12:13:04 -0600

    checking for token

--- app.rb
+++ app.rb
@@ -2,13 +2,16 @@ require 'sinatra'
 require 'json'
 
 post '/gateway' do
+
+  return if params[:token] != ENV['SLACK_TOKEN']
+
   trigger_word = params[:trigger_word].strip
   message = params[:text].gsub(trigger_word, '').strip
   user_name = params[:user_name].strip
 
   #switch on the message
-  # case message
-  #   when 'hello'
+  # case user_name
+  #   when ''
   #   else # default - ignore
   # end
 
